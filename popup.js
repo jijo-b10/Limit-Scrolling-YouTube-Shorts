249d318ee3900cae6e9e55a1da16cfb7ad483eca
jijo btkaya <119881826+jijo-b10@users.noreply.github.com>
2026-09-17 09:50:37 +0100

    new changes (#5)

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", () => {
 
+
+    const inputGroup = document.querySelector(".input-group");
+     function updateInputVisibility() {
+        inputGroup.style.display = toggle.checked ? "flex" : "none";
+    }
    const toggle = document.getElementById("check-5");
    chrome.storage.local.get(["enabled"], (res) => {
         toggle.checked = res.enabled !== false; // default ON
@@ -8,9 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
    toggle.addEventListener("change", () => {
         const newState = toggle.checked;
         chrome.storage.local.set({ 'enabled': newState });
-  
+        updateInputVisibility();
+
         if(!toggle.checked) {
              chrome.action.setIcon({path : "/turnOFF.png"},)
+             
                 }
         else {
              chrome.action.setIcon({ path : "/turnON.png" })
@@ -21,9 +29,15 @@ const input = document.getElementById('input-5');
 const dec = document.getElementById('decrement');
 const inc = document.getElementById('increment');
 
+
+chrome.storage.local.get(["shortCount"], (res) => {
+    input.value = (res.shortCount !== undefined) ? res.shortCount : 5;
+    refreshButtons();
+  });
   function refreshButtons() {
     dec.disabled = input.value <= 1;
     inc.disabled = input.value >= 10;
+    chrome.storage.local.set({ 'shortCount': input.value });
   }
 
   dec.addEventListener('click', () => {
@@ -36,7 +50,6 @@ const inc = document.getElementById('increment');
     refreshButtons();
   });
 
-  input.addEventListener('change', () => {
-    input.value = clamp(input.value);
+  input.addEventListener('input', () => {
     refreshButtons();
   });
